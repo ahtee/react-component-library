ced22ec6faa4db2225c7b5c6d335f1857407c563
Ben Otte <benottedev@gmail.com>
2019-06-17 18:18:59 -0400

    Develop (#6)
    * need to fix broken rollup config
    
    * adding bundlesize
    
    * adding fixed config, bundlesize and shield for travis build
    
    * fixing merge conflicts
    
    * committing new lock file

--- src/main.js
+++ src/main.js
@@ -1,2 +1,2 @@
-export { default as Button } from './components/Button';
-export { default as Card } from './components/Card';
+export { default as Button } from './components/Button/Button';
+export { default as Card } from './components/Card/Card';
