5465b02284d5e4360c60c2f28db1f89bcce942aa
212615385 <benjamin.otte@ge.com>
2019-06-17 17:38:13 -0400

    adding bundlesize

--- test/components/Button/Button.test.js
+++ test/components/Button/Button.test.js
@@ -1,6 +1,6 @@
 // Button.test.js
 // Licensed under the MIT license in the root directory of this project
 
-import Button from './Button';
+import Button from '../../../src/components/Button/Button';
 
 it('renders the button with props');
